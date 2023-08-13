140d2c2684d87711696aa6adfda87a7e22d7d0ae
martijor000 <82613327+martijor000@users.noreply.github.com>
2023-08-12 20:20:17 -0700

    Added Certification for ssl connection to mysql

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,10 @@
 using ReichertsMeatDistributing.Client.Services;
 using ReichertsMeatDistributing.Server;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +13,30 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
 builder.Services.AddScoped<IDealService, DealService>();
-builder.Services.AddHttpClient();
+// Configure HttpClient with custom certificate validation
+builder.Services.AddHttpClient<CustomHttpClient>(client =>
+{
+    // Configure other HttpClient settings if needed
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new CustomHttpClientHandler();
+
+    // Access the certificate and validate it
+    var certificatePath = "Certificate\\reichertsdistributing.der";
+    var certificateBytes = System.IO.File.ReadAllBytes(certificatePath);
+
+    handler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+    {
+        // Compare the certificate's raw bytes to the expected bytes
+        byte[] certificateRawBytes = cert.GetRawCertData();
+        return StructuralComparisons.StructuralEqualityComparer.Equals(certificateRawBytes, certificateBytes);
+    };
+
+    return handler;
+});
+
+
 var conn = builder.Configuration.GetConnectionString("Default");
 
 var app = builder.Build();
@@ -32,6 +60,24 @@ app.UseRouting();
 
 app.MapRazorPages();
 app.MapControllers();
+
+
 app.MapFallbackToFile("index.html");
 
 app.Run();
+
+public class CustomHttpClient : HttpClient
+{
+    public CustomHttpClient(HttpClientHandler handler) : base(handler)
+    {
+    }
+}
+
+public class CustomHttpClientHandler : HttpClientHandler
+{
+    public CustomHttpClientHandler()
+    {
+        // No need to instantiate the certificate validation here
+        // It is done within the ConfigurePrimaryHttpMessageHandler
+    }
+}
