aeabaffe47809722608ed0edff9db61866391d79
martijor000 <82613327+martijor000@users.noreply.github.com>
2025-01-18 11:49:02 -0800

    Added search bar work in progress

--- Client/Pages/Products.razor.cs
+++ Client/Pages/Products.razor.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using ReichertsMeatDistributing.Shared;
-using System.Net.Http.Json;
 using ReichertsMeatDistributing.Client.Services;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ReichertsMeatDistributing.Client.Pages
@@ -10,6 +11,8 @@ namespace ReichertsMeatDistributing.Client.Pages
     public partial class Products
     {
         private List<ProductItem>? productList;
+        private List<ProductItem>? filteredProducts;
+        private string searchQuery = string.Empty;
 
         [Inject]
         private IProductsService _productService { get; set; }
@@ -17,13 +20,35 @@ namespace ReichertsMeatDistributing.Client.Pages
         protected override async Task OnInitializedAsync()
         {
             await GetProducts();
-            productList = _productService._products;
+            FilterProducts();
         }
 
         private async Task GetProducts()
         {
             await _productService.GetProducts();
+            productList = _productService._products;
+            filteredProducts = productList;
+        }
 
+        private void HandleInput(ChangeEventArgs e)
+        {
+            searchQuery = e.Value.ToString();
+            FilterProducts();
+        }
+
+        private void FilterProducts()
+        {
+            if (productList != null)
+            {
+                filteredProducts = productList
+                    .Where(p => p.Description.Contains(searchQuery, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
         }
     }
 }
+
+
+
+
+
